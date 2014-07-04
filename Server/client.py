7627f49c5b5fafe7c57ea8a107db683a2243937e
GiapNH <hgbk.it@gmail.com>
2014-07-04 11:56:20 +0700

    no message

--- Server/client.py
+++ Server/client.py
@@ -63,7 +63,7 @@ def send(sock, cmd):
     sock.sendall(cmd.get_bytes())
     pass
 
-HOST, PORT = "192.168.1.179", 9090
+HOST, PORT = "localhost", 5555
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.connect((HOST, PORT))
 # cmd = Command(Command.CMD_ADD_FRIEND)
