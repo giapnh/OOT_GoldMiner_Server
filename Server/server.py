50d8151302b55851feb57c58491b17f576d27ae4
GiapNH <hgbk.it@gmail.com>
2014-07-14 16:06:24 +0700

    no message

--- Server/server.py
+++ Server/server.py
@@ -361,7 +361,7 @@ def analysis_message_remove_friend(sock, cmd):
         send(sock, send_cmd)
         send_cmd = Command(Command.CMD_REMOVE_FRIEND)
         send_cmd.add_string(Argument.ARG_PLAYER_USERNAME, sock_name_map.get(sock))
-        send_cmd.add_int(Argument.ARG_CODE, 1)
+        send_cmd.add_int(Argument.ARG_CODE, 2)
         send_cmd.add_string(Argument.ARG_MESSAGE, "You are removed friendship with " + str(sock_name_map.get(sock)) +
                             " !")
         send(name_sock_map.get(cmd.get_string(Argument.ARG_PLAYER_USERNAME)), send_cmd)
