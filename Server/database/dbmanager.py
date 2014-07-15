5866e65a4af3406f2d6049d137d6111ca7bedd64
GiapNH <hgbk.it@gmail.com>
2014-07-15 15:30:54 +0700

    Sap xep danh sach friend theo thu tu giam dan cua cup

--- Server/database/dbmanager.py
+++ Server/database/dbmanager.py
@@ -109,7 +109,7 @@ class DBManager:
             c = self.db.cursor()
             c.execute("""SELECT username,level,cup
             FROM user, friendship WHERE user.id = friendship.userid_1
-            and friendship.userid_2 = %s LIMIT %s OFFSET %s
+            and friendship.userid_2 = %s LIMIT %s OFFSET %s ORDER BY cup DESC
             """, (u_id, limit, offset, ))
             list_friend = {}
             for row in c:
